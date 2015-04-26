a8299445b33bc6eaaec21a99e386f65afa497cfe
Helibum1 <helibum1@gmail.com>
2015-04-26 13:55:03 +0200

    Update ScrollWheelHandler.java

--- src/main/ScrollWheelHandler.java
+++ src/main/ScrollWheelHandler.java
@@ -10,7 +10,7 @@ import main.log.Log;
  * Class that handles the input of the user. It stores the keys that are
  * currently pressed.
  */
-public class InputHandler {
+public class ScrollWheelHandler {
 	
 	/** A set containing all keys that are pressed. */
 	private final Set<Integer> keyPressed = new HashSet<Integer>();
@@ -26,60 +26,10 @@ public class InputHandler {
 	 * @param keyEvent
 	 *            The keyEvent that was triggered by the user.
 	 */
-	public void handleKeyEvent(KeyEvent keyEvent) {
+	public void handleMouseWheelEvent(MousWheelEvent wheelEvent) {
 
 		// The code of the key that is associated with this event.
-		int key =  keyEvent.getKeyCode();
-		
-		switch(keyEvent.getID()) {
-		
-		case KeyEvent.KEY_PRESSED: 
-			Log.debug("Pressed key: " + key);
-			keyPressed.add(key);
-			break;
-			
-		case KeyEvent.KEY_RELEASED: 
-			Log.debug("Released key: " + key);
-			keyReleased.add(key);
-			break;
-		
-		default: 
-			break;
-		}
-		
-	}
-	
-	/**
-	 * Check if a certain key is pressed.
-	 * 
-	 * @param keyCode
-	 *            The keyCode to check.
-	 * @return True, if the key is pressed.
-	 */
-	public boolean isKeyPressed(int keyCode) {
-		
-		return keyPressed.contains(keyCode);
-	}
-	
-	/**
-	 * Helper function to remove all keys from the keyPressed set
-	 * that have been released since the last iteration of the game
-	 * loop.<br>
-	 * <br>
-	 * This enables us to see very short keypresses also, namely the
-	 * ones that were pressed and released within one iteration of the
-	 * game loop.
-	 */
-	public void updatedReleasedKeys() {
-		
-		// Remove all keys that have been released since we last checked.
-		for (int keyCode : keyReleased) {
-			keyPressed.remove(keyCode);
-		}
-		
-		// Clear the keys that are released, since
-		// they are handled now.
-		keyReleased.clear();
+		int scroll =  keyEvent.getUnitsToScroll();
 		
 	}
 
