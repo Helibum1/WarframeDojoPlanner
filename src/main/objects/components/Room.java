0fdfdaaca5ade088f64322ccf0341d6b06be531c
KaMiKaZe642 <kamikaze6424.doggames@gmail.com>
2015-04-26 14:13:20 +0200

    Finished: 	- Room object 	- Parser for loading rooms from the database

--- src/main/objects/components/Room.java
+++ src/main/objects/components/Room.java
@@ -1,33 +1,105 @@
 package main.objects.components;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.awt.Point;
+import java.util.HashMap;
+import java.util.List;
 
 public class Room {
 	
-	public Room(String identifyer) {
-		setProperties(Loader.getProperties(identifyer));
-	}
-
-	private void setProperties(String[] properties) {
+	public enum Material {
+		credits, alloyPlate, circuits, controlModules, ferrite, gallium, morphics, neuralSensors, 
+		neurodes, orokinCells, rubedo, salvage, plastids, polymerBundle, argonCrystal, cryotic, 
+		oxium, tellurium, detoniteAmpule, fieldronSample, mutagenSample, fieldron, mutagenMass, 
+		space, energy
 	}
 	
-}
-
-class Loader {
-	public static String[] getProperties(String identifyer) {
-		String tmp = "";
-		try {
-			BufferedReader r = new BufferedReader(new FileReader(new File(identifyer)));
-			while (r.readLine() != null) {
-				tmp += r.readLine();
-			}
-			r.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return tmp.split("\n");
+	String name;
+	HashMap<String, HashMap<Material, Integer>> costs;
+	int decoCap;
+	
+	int counter = 0;
+	List<Point> doors;
+	
+	
+	public Room(String identifyer) {
+		setProperties(Loader.getProperties(identifyer));
+		costs.put("ghost", new HashMap<Material, Integer>());
+		costs.put("shadow", new HashMap<Material, Integer>());
+		costs.put("storm", new HashMap<Material, Integer>());
+		costs.put("mountain", new HashMap<Material, Integer>());
+		costs.put("moon", new HashMap<Material, Integer>());
 	}
+
+	void setProperties(String[] properties) {
+		for (int i = 0; i < properties.length; i++) {
+			switch (properties[i].split(":")[0]) {
+			case "name": name = properties[i].split(":")[1];break;
+			case "doors": setDoors(properties[i].split(":")[1]);break;
+			case "costs": setCosts(properties[i].split(":")[1]);break;
+			case "deco-cap": decoCap = Integer.parseInt(properties[i].split(":")[1]);break;
+			case "storm": override("storm", properties[i].split(":")[1]);break;
+			case "shadow": override("shadow", properties[i].split(":")[1]);break;
+			case "mountain": override("mountain", properties[i].split(":")[1]);break;
+			case "moon": override("moon", properties[i].split(":")[1]);break;
+			}
+		}
+	}
+	
+	void override(String level, String str) {
+		costs.get("level").put(Material.valueOf(str.split(" ")[0]), Integer.parseInt(str.split(" ")[1]));
+	}
+	
+	void setDoors(String str) {
+		String[] arr = str.split(";");
+		doors.clear();
+		for (String arst : arr) {
+			doors.add(new Point(Integer.parseInt(arst.split(",")[0]), Integer.parseInt(arst.split(",")[1])));
+		}
+	}
+	
+	void setCosts(String str) {
+		String[] arr = str.split(",");
+		costs.get("ghost").clear();
+		costs.get("shadow").clear();
+		costs.get("storm").clear();
+		costs.get("mountain").clear();
+		costs.get("moon").clear();
+		for (String tmp : arr) {
+			costs.get("ghost").put(Material.valueOf(tmp.split(" ")[0]), Integer.parseInt(tmp.split(" ")[1]));
+		}
+		for (String tmp : arr) {
+			costs.get("shadow").put(Material.valueOf(tmp.split(" ")[0]), Integer.parseInt(tmp.split(" ")[1])*3);
+		}
+		for (String tmp : arr) {
+			costs.get("storm").put(Material.valueOf(tmp.split(" ")[0]), Integer.parseInt(tmp.split(" ")[1])*10);
+		}
+		for (String tmp : arr) {
+			costs.get("mountain").put(Material.valueOf(tmp.split(" ")[0]), Integer.parseInt(tmp.split(" ")[1])*30);
+		}
+		for (String tmp : arr) {
+			costs.get("moon").put(Material.valueOf(tmp.split(" ")[0]), Integer.parseInt(tmp.split(" ")[1])*100);
+		}
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getDecoCap() {
+		return decoCap;
+	}
+	
+	public Point getNextDoor() {
+		counter++;
+		if (counter >= doors.size() || counter < 0) {
+			counter = 0;
+		}
+		return doors.get(counter);
+	}
+	
+	public Point getDefaultDoor() {
+		counter = 0;
+		return doors.get(counter);
+	}
+	
 }
