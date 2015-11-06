477d42ce2d4f0cf77a2fb06a547ebc393fde9018
Helibum1 <lukas.boner@stud.ksimlee.ch>
2015-11-06 17:21:59 +0100

    Added more javadoc comments

--- src/main/objects/components/Door.java
+++ src/main/objects/components/Door.java
@@ -19,11 +19,18 @@ public class Door extends Point {
 		return owith != null;
 	}
 	
+	/**
+	 * Connects this door to another room
+	 * @param r The room this door gets attached to
+	 */
 	public void attach(Room r) {
 		owith = r;
 		b.setEnabled(false);
 	}
 	
+	/**
+	 * The attached room got destroyed -> delete the room from this door and enable the button to create a new room at this location
+	 */
 	public void destroyAttached() {
 		owith = null;
 		b.setEnabled(true);
--- src/main/objects/components/Loader.java
+++ src/main/objects/components/Loader.java
@@ -8,6 +8,11 @@ import java.io.IOException;
 import main.log.Log;
 
 public class Loader {
+	/**
+	 * Loads the properties of a room from the file associated with it
+	 * @param identifyer The identifyer of the room to be loaded. Is the same as the name of the file of this room
+	 * @return A string-array with the properties of the room which was loaded
+	 */
 	public static String[] getProperties(String identifyer) {
 		Log.debug("Starting to read config for " + identifyer);
 		String tmp = "";
